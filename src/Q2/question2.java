ddd1fe264a135cf9b0a5b3193b54b9f630ac081c
mr.x <yingheting@gmail.com>
2019-11-18 23:01:06 +0800

    Question-2 recommit

--- src/Q2/question2.java
+++ src/Q2/question2.java
@@ -1,5 +1,8 @@
 package Q2;
 
+import javafx.css.StyleOrigin;
+
+import java.util.ArrayList;
 import java.util.Scanner;
 
 /**
@@ -10,11 +13,35 @@ public class question2
 {
 	public static void main(String[] args)
 	{
-		System.out.println("Please input positive integer:");
+		System.out.println("Please input a positive integer:");
 		Scanner sc = new Scanner(System.in);
+		String currentInput;
+		ArrayList<Integer> store = new ArrayList<>();
 		while (true)
 		{
-
+			currentInput = sc.nextLine();
+			try
+			{
+				if (Integer.parseInt(currentInput) > 0)
+				{
+					int temp = Integer.parseInt(currentInput);
+					for (int i = 1; i < temp; i++)
+					{
+						if (i % 2 != 0)
+						{
+							store.add(i);
+						}
+					}
+					System.out.println((store.size() == 0) ? "None" : store.toString());
+					store.clear();
+				} else
+				{
+					System.out.println("ERROR! Please input a POSITIVE integer!");
+				}
+			} catch (Exception e)
+			{
+				System.out.println("ERROR! Please input a INTEGER! NOT String!");
+			}
 		}
 	}
 }
