f32be0546f53a1b66768c57d3fc3c56588f69639
mr.x <yingheting@gmail.com>
2019-11-18 22:36:56 +0800

    Question-3 commit

---
+++ src/Q2/question2.java
@@ -0,0 +1,20 @@
+package Q2;
+
+import java.util.Scanner;
+
+/**
+ * @author paulalan
+ * @create 2019/11/18 22:20
+ */
+public class question2
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Please input positive integer:");
+		Scanner sc = new Scanner(System.in);
+		while (true)
+		{
+
+		}
+	}
+}
---
+++ src/Q3/Shape.java
@@ -0,0 +1,12 @@
+package Q3;
+
+public abstract class Shape
+{
+	public double area = 0;
+	public double circumference = 0;
+
+
+	public abstract void shapeCalculate();
+
+
+}
